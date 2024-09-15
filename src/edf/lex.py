4f531bd0d33f60691ee016244ef20c7b7ad9d343
Tom Savage <tcsavage@gmail.com>
2024-09-15 09:34:56 +0100

    Add new laxer and parser base (incomplete)

---
+++ src/edf/lex.py
@@ -0,0 +1,368 @@
+import re
+from dataclasses import dataclass, field
+from enum import Enum
+
+
+@dataclass
+class LexicalError(BaseException):
+    offset: int
+    line: int
+    col: int
+    message: str
+
+
+class TokenId(Enum):
+    EOF = "EOF"
+    LPAREN = "LPAREN"
+    RPAREN = "RPAREN"
+    LBRACE = "LBRACE"
+    RBRACE = "RBRACE"
+    LBRACKET = "LBRACKET"
+    RBRACKET = "RBRACKET"
+    COMMA = "COMMA"
+    SEMICOLON = "SEMICOLON"
+    EQUALS = "EQUALS"
+    KW_TRUE = "KW_TRUE"
+    KW_FALSE = "KW_FALSE"
+    ID_NAME = "ID_NAME"
+    LIT_NUM_DEC = "LIT_NUM_DEC"
+    LIT_STRING = "LIT_STRING"
+
+
+left_delimiter_to_right_delimiter = {
+    TokenId.LPAREN: TokenId.RPAREN,
+    TokenId.LBRACE: TokenId.RBRACE,
+    TokenId.LBRACKET: TokenId.RBRACKET,
+}
+
+right_delimiters = set(left_delimiter_to_right_delimiter.values())
+
+
+id_name_pattern = re.compile(r"[a-z_][a-zA-Z0-9'_]*#?")
+lit_num_pattern = re.compile(r"-?[1-9][0-9]*(\.[0-9]+)?#?")
+lit_string_pattern = re.compile(r'("(?!"").*?(?<!\\)(\\\\)*?")')
+
+
+def is_word_char(c: str) -> bool:
+    return c.isalnum() or c in "_#'"
+
+
+@dataclass(frozen=True)
+class Token:
+    id: TokenId
+    value: str
+    offset: int
+    size: int
+    line: int
+    col: int
+    fabricated: bool = False
+    error: bool = False
+
+
+@dataclass
+class BraceBlock:
+    open_token: Token
+    item_indentation: int | None = None
+
+
+@dataclass
+class LexicalAnalyzer:
+    # Source to lex.
+    source: str
+
+    # Lexer position state.
+    offset: int = 0
+    line: int = 1
+    col: int = 1
+
+    # Right delimiters we expect to close, in order.
+    open_delimiters: list[TokenId] = field(default_factory=list)
+
+    # Records if we have yet to emit a token on the current line.
+    first_token_on_line: bool = True
+
+    # Records the indentation of the current line.
+    line_indent: int = 0
+
+    # Records the current brace block stack.
+    brace_block_stack: list[BraceBlock] = field(default_factory=list)
+
+    # The tokens we've emitted.
+    tokens: list[Token] = field(default_factory=list)
+
+    # Configuration.
+    emit_semicolon_on_eof: bool = True
+    strict: bool = False
+
+    def match_token(self, token_map: list[tuple[str, TokenId]], word: bool = False) -> Token | None:
+        for token_str, token_id in token_map:
+            if self.source.startswith(token_str, self.offset) and (
+                not word
+                or not is_word_char(
+                    self.source[min(self.offset + len(token_str), len(self.source) - 1)]
+                )
+            ):
+                token = Token(token_id, token_str, self.offset, len(token_str), self.line, self.col)
+                self.offset += len(token_str)
+                self.col += len(token_str)
+                return token
+        return None
+
+    def emit_token(self, token: Token):
+        """
+        Emits a token, handling any extra token insertion logic.
+        - Auto inserts matching right delimiters
+        - Auto inserts semicolons where needed
+        """
+
+        # Right delimiter insertion logic.
+        # Whenever we emit a left delimiter (i.e. one of "(", "{", "["), we push
+        # the corresponding right delimiter to the open_delimiters stack.
+        # Whenever we emit a right delimiter (i.e. one of ")", "}", "]"), we
+        # check the top of the open_delimiters stack. If it doesn't match, we
+        # emit "fabricated" right delimiters until the stack is empty or the
+        # top of the stack matches the right delimiter.
+        # Then we can emit the original token.
+        # A similar process is performed for and open delimiters when we
+        # encounter EOF.
+        if token.id in left_delimiter_to_right_delimiter:
+            # Push right delimiter to open_delimiters stack.
+            right_delimiter = left_delimiter_to_right_delimiter[token.id]
+            self.open_delimiters.append(right_delimiter)
+        elif token.id in right_delimiters:
+            # We have a right delimiter, so let's close any non-closed delimiters that don't match.
+            if not self.strict:
+                while self.open_delimiters and self.open_delimiters[-1] != token.id:
+                    right_delimiter = self.open_delimiters.pop()
+                    self.tokens.append(
+                        Token(
+                            right_delimiter,
+                            "",
+                            self.offset,
+                            0,
+                            self.line,
+                            self.col,
+                            fabricated=True,
+                            error=True,
+                        )
+                    )
+            # Pop the matching open delimiter.
+            if self.open_delimiters:
+                assert self.open_delimiters[-1] == token.id
+                self.open_delimiters.pop()
+        elif token.id == TokenId.EOF:
+            # Close any open delimiters.
+            while self.open_delimiters:
+                right_delimiter = self.open_delimiters.pop()
+                if right_delimiter == TokenId.RBRACE:
+                    self.brace_block_stack.pop()
+                    # if self.tokens[-1].id != TokenId.SEMICOLON:
+                    #     self.tokens.append(
+                    #         Token(
+                    #             TokenId.SEMICOLON,
+                    #             "",
+                    #             self.offset,
+                    #             0,
+                    #             self.line,
+                    #             self.col,
+                    #             fabricated=True,
+                    #         )
+                    #     )
+                self.tokens.append(
+                    Token(
+                        right_delimiter,
+                        "",
+                        self.offset,
+                        0,
+                        self.line,
+                        self.col,
+                        fabricated=True,
+                        error=True,
+                    )
+                )
+            # # Add a semicolon to the end of the file.
+            # if (
+            #     self.tokens
+            #     and self.tokens[-1].id != TokenId.SEMICOLON
+            #     and self.emit_semicolon_on_eof
+            # ):
+            #     self.tokens.append(
+            #         Token(
+            #             TokenId.SEMICOLON, "", self.offset, 0, self.line, self.col, fabricated=True
+            #         )
+            #     )
+
+        # Core of semicolon insertion.
+        # The grammar of the language uses semicolons to terminate attribute
+        # definitions within blocks. However, we don't want to require the user
+        # to have to actually provide them. Instead, a newline can terminate
+        # an attribute definition and we can insert the semicolon for them.
+        # Whenever we open a brace block (i.e. on emitting a "{" token), we
+        # push a BraceBlock object to the brace_block_stack. This object
+        # records the necessary information to determine if a semicolon
+        # should be inserted.
+        # Note: this occurs AFTER the right delimiter insertion logic.
+        # FIXME: A closing brace fabricated by the lexer does not currently
+        # interact with this logic.
+        if token.id == TokenId.LBRACE:
+            # Opening a brace block.
+            self.brace_block_stack.append(BraceBlock(token))
+        elif token.id == TokenId.RBRACE:
+            # Closing a brace block.
+            # Add a semicolon to end.
+            if self.brace_block_stack:
+                self.brace_block_stack.pop()
+                # If the last token was already a semicolon, don't add another.
+                if self.tokens[-1].id not in {TokenId.SEMICOLON, TokenId.RBRACE, TokenId.LBRACE}:
+                    self.tokens.append(
+                        Token(
+                            TokenId.SEMICOLON,
+                            "",
+                            self.offset,
+                            0,
+                            self.line,
+                            self.col,
+                            fabricated=True,
+                        )
+                    )
+        elif self.first_token_on_line and (self.tokens[-1].id not in {TokenId.RBRACE} if self.tokens else True):
+            # If we're at the start of a line, we _may_ need to insert a
+            # semicolon. Specifically if we're inside a brace block and the
+            # token's indentation is <= the block's indentation.
+            # When a brace block is first opened, it has no indentation. In
+            # this case we set the indentation to the token's column.
+            # If the previous token was a closing brace, we don't insert a
+            # semicolon.
+            # Otherwise, if the token's column is > the block's indentation,
+            # we consider the current line to be an extension of the previous
+            # line and don't insert a semicolon. (Offside rule)
+            if self.brace_block_stack:
+                if self.brace_block_stack[-1].item_indentation is None:
+                    # In this case the top brace block has no indentation, so we set it.
+                    self.brace_block_stack[-1].item_indentation = token.col
+                elif (
+                    token.col <= self.brace_block_stack[-1].item_indentation
+                    and self.tokens[-1].id != TokenId.SEMICOLON
+                ):  # Should this be == and auto-close on <?
+                    self.tokens.append(
+                        Token(
+                            TokenId.SEMICOLON,
+                            "",
+                            self.offset,
+                            0,
+                            self.line,
+                            self.col,
+                            fabricated=True,
+                        )
+                    )
+            # elif self.col == 0 and self.tokens and self.tokens[-1].id != TokenId.SEMICOLON:
+            #     # If we're not in a brace block and the token is at the start of a line, insert a semicolon.
+            #     # This is for top-level definitions.
+            #     self.tokens.append(
+            #         Token(
+            #             TokenId.SEMICOLON, "", self.offset, 0, self.line, self.col, fabricated=True
+            #         )
+            #     )
+
+        # Finally we can add the original input token to the output.
+        # The EOF token is special and is not actually added to the token list.
+        if token.id != TokenId.EOF:
+            self.tokens.append(token)
+
+    def read_token(self) -> bool:
+        while True:
+            if self.offset >= len(self.source):
+                self.emit_token(Token(TokenId.EOF, "", self.offset, 0, self.line, self.col))
+                return False
+            c = self.source[self.offset]
+            if self.source.startswith("\r\n", self.offset):
+                self.line += 1
+                self.col = 1
+                self.offset += 2
+                self.first_token_on_line = True
+            elif c in {"\r", "\n"}:
+                self.line += 1
+                self.col = 1
+                self.offset += 1
+                self.first_token_on_line = True
+            elif c.isspace():
+                self.offset += 1
+                self.col += 1
+            else:
+                break
+
+        if self.first_token_on_line:
+            self.line_indent = self.col
+
+        if tok := self.match_token(
+            [
+                ("(", TokenId.LPAREN),
+                (")", TokenId.RPAREN),
+                ("{", TokenId.LBRACE),
+                ("}", TokenId.RBRACE),
+                ("[", TokenId.LBRACKET),
+                ("]", TokenId.RBRACKET),
+                (",", TokenId.COMMA),
+                (";", TokenId.SEMICOLON),
+                ("=", TokenId.EQUALS),
+            ]
+        ):
+            self.emit_token(tok)
+        elif tok := self.match_token(
+            [
+                ("true", TokenId.KW_TRUE),
+                ("false", TokenId.KW_FALSE),
+            ],
+            word=True,
+        ):
+            self.emit_token(tok)
+        elif match := id_name_pattern.match(self.source, self.offset):
+            size = len(match.group(0))
+            self.emit_token(
+                Token(TokenId.ID_NAME, match.group(0), self.offset, size, self.line, self.col)
+            )
+            self.offset += size
+            self.col += size
+        elif match := lit_num_pattern.match(self.source, self.offset):
+            size = len(match.group(0))
+            self.emit_token(
+                Token(TokenId.LIT_NUM_DEC, match.group(0), self.offset, size, self.line, self.col)
+            )
+            self.offset += size
+            self.col += size
+        elif match := lit_string_pattern.match(self.source, self.offset):
+            size = len(match.group(0))
+            self.emit_token(
+                Token(TokenId.LIT_STRING, match.group(0), self.offset, size, self.line, self.col)
+            )
+            self.offset += size
+            self.col += size
+        else:
+            raise LexicalError(self.offset, self.line, self.col, f"Unexpected character {repr(self.source[self.offset])}")
+        self.first_token_on_line = False
+        return True
+
+
+def rebuild_string(source: str, tokens: list[Token]) -> str:
+    return "·".join(source[token.offset : token.offset + token.size] for token in tokens)
+
+
+def tokenize(source: str, emit_semicolon_on_eof: bool = True) -> list[Token]:
+    lexer = LexicalAnalyzer(source, emit_semicolon_on_eof=emit_semicolon_on_eof)
+    while lexer.read_token():
+        pass
+    return lexer.tokens
+
+
+def test(string: str) -> str:
+    tokens = tokenize(string)
+    return rebuild_string(string, tokens)
+
+
+if __name__ == "__main__":
+    import sys
+
+    text = sys.stdin.read()
+    toks = tokenize(text)
+    for tok in toks:
+        print(tok.value or tok.id.value)
---
+++ src/edf/parse.py
@@ -0,0 +1,251 @@
+from collections.abc import Container, MutableSequence, Sequence, Iterable
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+from edf.lex import Token, TokenId
+
+
+class NodeId(Enum):
+    BLOCK_INTRODUCER = "BLOCK_INTRODUCER"
+    BLOCK_ID = "BLOCK_ID"
+    BLOCK_BODY_START = "BLOCK_BODY_START"
+    BLOCK_BODY_END = "BLOCK_BODY_END"
+    BLOCK = "BLOCK"
+    ATTRIBUTE_INTRODUCER = "ATTRIBUTE_INTRODUCER"
+    ATTRIBUTE_ASSIGNMENT = "ATTRIBUTE_ASSIGNMENT"
+    ATTRIBUTE = "ATTRIBUTE"
+    LIT_STRING = "LIT_STRING"
+    LIT_NUMBER = "LIT_NUMBER"
+
+
+@dataclass
+class NodeKind:
+    id: NodeId
+    fixed_num_children: Optional[int] = 0
+    bracket: Optional["NodeKind"] = None
+
+    def __post_init__(self):
+        if self.bracket is not None and self.fixed_num_children == 0:
+            self.fixed_num_children = None
+        if self.fixed_num_children is None and self.bracket is None:
+            raise ValueError("Must specify either fixed_num_children or bracket")
+        if self.fixed_num_children is not None and self.bracket is not None:
+            raise ValueError("Cannot specify both fixed_num_children and bracket")
+        
+
+node_block_introducer = NodeKind(NodeId.BLOCK_INTRODUCER)
+node_block_id = NodeKind(NodeId.BLOCK_ID)
+node_block_body_start = NodeKind(NodeId.BLOCK_BODY_START)
+node_block = NodeKind(NodeId.BLOCK, bracket=node_block_introducer)
+
+node_attribute_introducer = NodeKind(NodeId.ATTRIBUTE_INTRODUCER)
+node_attribute_assignment = NodeKind(NodeId.ATTRIBUTE_ASSIGNMENT)
+node_attribute = NodeKind(NodeId.ATTRIBUTE, bracket=node_attribute_introducer)
+
+node_lit_string = NodeKind(NodeId.LIT_STRING)
+node_lit_number = NodeKind(NodeId.LIT_NUMBER)
+
+
+node_kinds = {
+    NodeId.BLOCK_INTRODUCER: node_block_introducer,
+    NodeId.BLOCK_ID: node_block_id,
+    NodeId.BLOCK_BODY_START: node_block_body_start,
+    NodeId.BLOCK: node_block,
+    NodeId.ATTRIBUTE_INTRODUCER: node_attribute_introducer,
+    NodeId.ATTRIBUTE_ASSIGNMENT: node_attribute_assignment,
+    NodeId.ATTRIBUTE: node_attribute,
+    NodeId.LIT_STRING: node_lit_string,
+    NodeId.LIT_NUMBER: node_lit_number,
+}
+
+
+@dataclass
+class Node:
+    kind: NodeKind
+    token: Token
+
+
+class StateId(Enum):
+    DOC = "DOC"
+    BLOCK_INTRODUCER = "BLOCK_INTRODUCER"
+    BLOCK_NAMED = "BLOCK_NAMED"
+    BLOCK_BODY_UNKNOWN = "BLOCK_BODY_UNKNOWN"
+    BLOCK_BODY_VALUE = "BLOCK_BODY_VALUE"
+    BLOCK_BODY_AGGREGATE = "BLOCK_BODY_AGGREGATE"
+    ATTRIBUTE_INTRODUCER = "ATTRIBUTE_INTRODUCER"
+    ATTRIBUTE_ASSIGNMENT = "ATTRIBUTE_ASSIGNMENT"
+    ATTRIBUTE_VALUE = "ATTRIBUTE_VALUE"
+    VALUE = "VALUE"
+
+
+@dataclass
+class State:
+    id: StateId
+    start_token_idx: int
+
+
+class Parser:
+    tokens: Sequence[Token]
+    tree: MutableSequence[Node]
+    state_stack: list[State]
+    token_index: int
+
+    def __init__(self, tokens: Sequence[Token]):
+        self.tokens = tokens
+        self.tree = []
+        self.state_stack = [State(StateId.DOC, 0)]
+        self.token_index = 0
+
+    def push_state(self, state: State):
+        self.state_stack.append(state)
+
+    def pop_state(self, expected_state_ids: Optional[Container[StateId]] = None) -> State:
+        state = self.state_stack.pop()
+        if expected_state_ids and state.id not in expected_state_ids:
+            raise ValueError(f"Unexpected state {state.id}")
+        return state
+        
+    def consume(self, one_of: Optional[Sequence[TokenId]] = None) -> Token:
+        """
+        Returns the current token and advances the token index.
+        If `one_of` is provided, raises an error if the current token's ID is not in the list.
+        """
+        token = self.tokens[self.token_index]
+        if one_of and token.id not in one_of:
+            raise ValueError(f"Unexpected token {token.id}")
+        self.token_index += 1
+        return token
+    
+    def consume_if(self, token_id: TokenId) -> bool:
+        """
+        Consumes the current token if it matches the given ID.
+        Returns True if the token was consumed, False otherwise.
+        """
+        if self.tokens[self.token_index].id == token_id:
+            self.token_index += 1
+            return True
+        return False
+    
+    def consume_discard(self):
+        self.token_index += 1
+
+    def emit_node(self, node: Node):
+        self.tree.append(node)
+
+    def emit_leaf_node(self, node_kind: NodeKind, token: Token):
+        self.emit_node(Node(node_kind, token))
+
+    def emit_compound_node(self, node_kind: NodeKind, token: Token, start_token_idx: int):
+        self.emit_node(Node(node_kind, token))
+
+    def step(self):
+        print(f"Stepping at token {self.token_index} ({self.tokens[self.token_index].id}). State: {self.state_stack[-1].id}")
+        if not self.state_stack:
+            raise ValueError("Empty stack")
+        state = self.state_stack[-1]
+        match state.id, self.tokens[self.token_index].id:
+            case StateId.DOC, TokenId.ID_NAME:
+                # The root level. We encounter a block introducer.
+                # So lets push the block introducer state and emit a node, consuming the token.
+                token = self.consume()
+                self.push_state(State(StateId.BLOCK_INTRODUCER, self.token_index))
+                self.emit_node(Node(node_block_introducer, token))
+            case StateId.BLOCK_INTRODUCER, TokenId.ID_NAME:
+                self.pop_state()
+                state.id = StateId.BLOCK_NAMED
+                self.push_state(state)
+                self.emit_node(Node(node_block_id, self.consume()))
+            case StateId.BLOCK_INTRODUCER | StateId.BLOCK_NAMED, TokenId.LBRACE:
+                token = self.consume()
+                self.push_state(State(StateId.BLOCK_BODY_UNKNOWN, self.token_index))
+                self.emit_node(Node(node_block_body_start, token))
+            case StateId.BLOCK_BODY_UNKNOWN | StateId.BLOCK_BODY_VALUE | StateId.BLOCK_BODY_AGGREGATE, TokenId.RBRACE:
+                token = self.consume()
+                self.pop_state(expected_state_ids={StateId.BLOCK_BODY_UNKNOWN, StateId.BLOCK_BODY_VALUE, StateId.BLOCK_BODY_AGGREGATE})
+                state = self.pop_state(expected_state_ids={StateId.BLOCK_INTRODUCER, StateId.BLOCK_NAMED})
+                self.emit_compound_node(node_block, token, state.start_token_idx)
+            case StateId.BLOCK_BODY_UNKNOWN | StateId.BLOCK_BODY_AGGREGATE, TokenId.ID_NAME:
+                self.pop_state()
+                state.id = StateId.BLOCK_BODY_AGGREGATE
+                self.push_state(state)
+                token = self.consume()
+                self.push_state(State(StateId.ATTRIBUTE_INTRODUCER, self.token_index))
+                self.emit_node(Node(node_attribute_introducer, token))
+            case StateId.ATTRIBUTE_INTRODUCER, TokenId.EQUALS:
+                self.push_state(State(StateId.ATTRIBUTE_VALUE, self.token_index))
+                self.push_state(State(StateId.VALUE, self.token_index))
+                self.emit_leaf_node(node_attribute_assignment, self.consume())
+            case StateId.VALUE, TokenId.LIT_STRING | TokenId.LIT_NUM_DEC:
+                token = self.consume()
+                match token.id:
+                    case TokenId.LIT_STRING:
+                        self.emit_leaf_node(node_lit_string, token)
+                    case TokenId.LIT_NUM_DEC:
+                        self.emit_leaf_node(node_lit_number, token)
+                    case _:
+                        raise ValueError(f"Unexpected token {token.id}")
+                self.pop_state()
+            case StateId.ATTRIBUTE_VALUE, TokenId.SEMICOLON:
+                # Close the ATTRIBUTE_VALUE state and emit the ATTRIBUTE_ASSIGNMENT node.
+                self.pop_state()
+                self.pop_state([StateId.ATTRIBUTE_INTRODUCER])
+                self.emit_leaf_node(node_attribute, self.consume())
+                
+
+            
+
+    def build_tree(self):
+        while self.token_index < len(self.tokens):
+            self.step()
+        print(f"Final stack: {self.state_stack}")
+
+
+@dataclass
+class ExplicitTreeNode:
+    node: Node
+    children: Optional[Sequence["ExplicitTreeNode"]] = None
+
+
+def build_explicit_tree(nodes: Iterable[Node]) -> ExplicitTreeNode:
+    stack = []
+    for node in nodes:
+        print(f"Processing node {node.kind.id}")
+        if node.kind.fixed_num_children:
+            children = stack[-node.kind.fixed_num_children:]
+            stack[-node.kind.fixed_num_children:] = []
+            stack.append(ExplicitTreeNode(node, children))
+        elif node.kind.bracket:
+            idx = -1
+            try:
+                print(f"Looking for bracket {node.kind.bracket.id}")
+                while stack[idx].node.kind.id != node.kind.bracket.id:
+                    print(f"Inspected {stack[idx].node.kind.id} (looking for {node.kind.bracket.id})")
+                    idx -= 1
+            except IndexError:
+                raise ValueError(f"Could not find bracket for node {node.kind.id} (looking for {node.kind.bracket.id})")
+            children = stack[idx:]
+            stack[idx:] = []
+            stack.append(ExplicitTreeNode(node, children))
+        else:
+            stack.append(ExplicitTreeNode(node))
+    assert len(stack) == 1, f"Expected stack to have 1 element, got {len(stack)}"
+    return stack[0]
+
+
+if __name__ == "__main__":
+    import pprint
+    import sys
+    from edf.lex import tokenize
+
+    text = sys.stdin.read()
+    toks = tokenize(text)
+    parser = Parser(toks)
+    parser.build_tree()
+
+    print("========== TREE ==========")
+    for node in parser.tree:
+        print(node.kind.id.value, node.kind.fixed_num_children, "=>", node.token.id.value, node.token.value)
+
+    print("========== EXPLICIT TREE ==========")
+    pprint.pprint(build_explicit_tree(parser.tree))
